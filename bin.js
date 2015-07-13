d9f1754c7ea7b9d0b50446b12ba2f95fdf7a208f
Nate Goldman <nate@ngoldman.me>
2015-07-12 17:50:09 -0700

    :art: use streams

--- bin.js
+++ bin.js
@@ -2,16 +2,4 @@
 
 var sitedown = require('.')
 
-sitedown({
-  source: '.', // source directory
-  header: __dirname + '/partials/header.html', // path to header partial
-  footer: __dirname + '/partials/footer.html', // path to footer partial
-  output: './sitedown/' // path to output folder
-}, function (err) {
-  if (err) {
-    console.error(err)
-    process.exit(1)
-  }
-
-  console.log('✓ success')
-})
+sitedown()
--- index.js
+++ index.js
@@ -1,99 +1,84 @@
 var fs = require('fs')
+var path = require('path')
 var cwp = require('cwp')
-var glob = require('glob')
+var readdirp = require('readdirp')
+var mkdirp = require('mkdirp')
+var es = require('event-stream')
 var md = require('markdown-it')({
   html: true,
   linkify: true,
   typographer: true
 })
 var encoding = { encoding: 'utf8' }
-var ignore = ['node_modules']
+
+function noop () {}
 
 function sitedown (options, callback) {
+  options = options || {}
+  var root = options.root || cwp('.')
+  var header = options.header || ''
+  var footer = options.footer || ''
+  var output = options.output || cwp('./site/')
 
-  gatherFiles(options.source, function (err, files) {
-    if (err) return callback(err)
+  if (typeof callback === 'undefined') callback = noop
 
-    var filesOpts = {
-      files: files,
-      ignore: options.ignore || ignore
-    }
+  readdirp({
+    root: root,
+    fileFilter: ['*.md', '*.markdown'],
+    directoryFilter: ['!.git', '!node_modules']
+  })
+    .on('warn', function (err) {
+      console.error('non-fatal error', err)
+    })
+    .on('error', function (err) {
+      callback(err)
+    })
+    .pipe(es.mapSync(function (entry) {
+      return entry.path
+    }))
+    .on('data', function (file) {
+      var dest = file.split('/').slice(0, -1).join('/')
+      var name = file.split('.')[0].split('/').pop()
 
-    parseFiles(filesOpts, function (err, pages) {
-      if (err) return callback(err)
+      if (name === 'README') name = 'index'
 
-      var pagesOpts = {
-        pages: pages,
-        header: options.header,
-        footer: options.footer,
-        output: options.output
-      }
+      dest = path.join(dest, name + '.html')
 
-      buildPages(pagesOpts, function (err) {
-        if (err) return callback(err)
+      var pageBody = fileToPageBody(path.join(root, file))
+      var html = buildPage(header, pageBody, footer)
 
-        callback(null)
+      mkdirp.sync(output)
+
+      fs.writeFile(path.join(output, dest), html, encoding, function (err) {
+        if (err) return console.error(err)
+        console.log('built', dest)
       })
     })
-  })
-}
-
-function gatherFiles (pattern, callback) {
-  glob(pattern, function (err, files) {
-    if (err) return callback(err)
-    if (callback) callback(null, files)
-  })
-}
-
-function parseFiles (options, callback) {
-  var pages = []
-
-  options.files.forEach(function (file) {
-    // exclude any files that match a pattern from ignore
-    for (var j = 0; j < options.ignore.length; j++) {
-      if (file.match(options.ignore[j]) !== null) return
-    }
-
-    var path = file.split('/').slice(0, -1).join('/')
-    var name = file.split('.')[0].split('/').pop()
-
-    // treat README as directory index
-    if (name === 'README') name = 'index'
-
-    console.log(file, path, name)
-
-    // add page object to pages array
-    pages.push({
-      path: path + '/' + name + '.html',
-      body: md.render(fs.readFileSync(cwp('./' + file), encoding))
+    .on('end', function () {
+      callback(null, true)
     })
-  })
-
-  if (callback) callback(null, pages)
 }
 
-function buildPages (options, callback) {
+/**
+ * turns markdown file into html
+ * @param  {String}  filePath   full path to markdown file
+ * @return {Object}             html
+ */
+function fileToPageBody (filePath) {
+  var body = fs.readFileSync(filePath, encoding)
+  return md.render(body)
+}
 
-  options.pages.forEach(function (page) {
-    var header = fs.readFileSync(cwp(options.header), encoding)
-    var footer = fs.readFileSync(cwp(options.footer), encoding)
+/**
+ * glues together html header, body, and footer
+ * also rewrites markdown links to html links
+ */
+function buildPage (header, body, footer) {
+  header = header || ''
+  body = body ? body.replace(/(\.md|\.markdown)"/g, '.html"') : ''
+  footer = footer || ''
 
-    // rewrite all relative links to markdown files to point to html equivalents
-    var content = page.body.replace(/\.md"/g, '.html"')
-
-    var output = options.output
-    if (output[output.length - 1] !== '/' &&
-        page.path[0] !== '/') output += '/'
-    output += page.path
-
-    // stitch together header, content, and footer
-    fs.writeFile(cwp(output), header + content + footer, function (err) {
-      if (err) return console.error(err)
-      console.log('built', output)
-    })
-  })
-
-  callback(null, true)
+  return header + body + footer
 }
 
 module.exports = sitedown
