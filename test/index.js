22c098cff317851edbe2b94ddb518c88054da0d5
Nate Goldman <nate@ngoldman.me>
2015-07-21 12:50:30 -0700

    :white_check_mark: expand tests

--- test/index.js
+++ test/index.js
@@ -3,7 +3,31 @@ var path = require('path')
 var rimraf = require('rimraf')
 var test = require('tape')
 
-test('Example Test', function (t) {
+test('markdown to html', function (t) {
+  var sitedown = require('../')
+
+  var opts = {
+    root: path.join(__dirname, 'markdown'),
+    output: path.join(__dirname, 'site')
+  }
+
+  rimraf(opts.output, generateSite)
+
+  function generateSite () {
+    sitedown(opts, function (err) {
+      if (err) return console.error(err)
+
+      var generatedIndex = path.join(opts.output, 'index.html')
+      fs.readFile(generatedIndex, {encoding: 'utf8'}, function (err, data) {
+        if (err) t.error(err)
+        t.equals(data, '<h1>TESTING!</h1>\n', 'conversion lgtm')
+        t.end()
+      })
+    })
+  }
+})
+
+test('header & footer concatenation', function (t) {
   var sitedown = require('../')
 
   var opts = {
@@ -21,10 +45,10 @@ test('Example Test', function (t) {
 
       var generatedIndex = path.join(opts.output, 'index.html')
       fs.readFile(generatedIndex, {encoding: 'utf8'}, function (err, data) {
-        if (err) t.errir(err)
-        t.equals(data, opts.header + '<h1>TESTING!</h1>\n' + opts.footer)
+        if (err) t.error(err)
+        t.equals(data, opts.header + '<h1>TESTING!</h1>\n' + opts.footer, 'header, content, & footer are there')
+        t.end()
       })
-      t.end()
     })
   }
 })
